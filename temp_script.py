df35186a9aa968b38cad9d98a5e092abda8d519d
devkumar1109 <dev.kumar.met22@itbhu.ac.in>
2025-02-24 09:22:52 +0530

    DB Set Done

--- temp_script.py
+++ temp_script.py
@@ -14,12 +14,12 @@ db = client["my_articles"]
 collection = db["news_articles"]  # Collection where articles will be stored
 
 article = {}
-article['title'] = '''Love Turns to Blood: Fatal Affair at Maha Kumbh'''
-article['link'] = f"{project_path}/articles/Man_Kills_Wife_During_Kumbh_Tour/index.html"
-keywords = "news, latest news, updates, maha kumbh tour, alleged affair, throat slit, knife, murder, blood-stained clothes, dustbin, crime confession, missing person, wife killed, husband accused, domestic violence, marital dispute, kumbh festival, murder weapon, throat slashingcrime, uttar pradesh"
+article['title'] = '''Divine From Above: Viral Aerial Video Captures Varanasi's Timeless Essence'''
+article['link'] = f"{project_path}/articles/Varanasi's_Divine_Essence_Caught_on_Camera/index.html"
+keywords = "news, latest news, updates, varanasi, kashi, banaras, india, holy city, aerial video, drone footage, kashi vishwanath temple, manikarnika ghat, ganga aarti, spiritual essence, divine energy, viral video, social media, indian culture, religious tourism, drone shots, ganga riverbusiness, varanasi"
 keywords = [i.strip() for i in keywords.split(',')]
 article['keywords'] = keywords
-article['image'] = "https://res.cloudinary.com/duix0mwfx/image/upload/v1740350712/Man%20Kills%20Wife%20During%20Kumbh%20Tour.webp"
+article['image'] = "https://res.cloudinary.com/duix0mwfx/image/upload/v1740350743/Varanasi%27s%20Divine%20Essence%20Caught%20on%20Camera.webp"
 
 article_data = {
         "title": article["title"],
